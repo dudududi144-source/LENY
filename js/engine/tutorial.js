4b6887f76386ef0b9d7e1cd1ab0b79074c82bbbb
dudududu144 <dudududi144@gmail.com>
2026-08-22 19:36:09 +0300

    feat(infra #28): tutorial reads locale-aware strings (he/en)

--- js/engine/tutorial.js
+++ js/engine/tutorial.js
@@ -5,25 +5,25 @@ import {S,save} from '../core/state.js';
 import {AU} from '../core/audio.js';
 import {TTS} from '../core/tts.js';
 import {later} from '../core/utils.js';
-import STR from '../core/strings.js';
+import {tr} from '../core/strings.js';
 
 export function tutorialStart(li){
  RT.tut=0;RT.tutDist=0;
  if(li===0&&!S.tutorial){RT.tut=1;
-  later(()=>TTS.say(STR.tutorial.welcome),700);}}
+  later(()=>TTS.say(tr().tutorial.welcome),700);}}
 
 export function tutorialUpdate(p){
  if(RT.tut===1){
   RT.tutDist=(RT.tutDist||0)+Math.abs(p.vx);
-  RT.curHint=STR.tutorial.walkHint;RT.hintTimer=200;
+  RT.curHint=tr().tutorial.walkHint;RT.hintTimer=200;
   if(RT.tutDist>130){RT.tut=2;AU.sfx('power');
-   TTS.say(STR.tutorial.jumpPraise);}}
+   TTS.say(tr().tutorial.jumpPraise);}}
  else if(RT.tut===2){
-  RT.curHint=STR.tutorial.jumpHint;RT.hintTimer=200;
+  RT.curHint=tr().tutorial.jumpHint;RT.hintTimer=200;
   if(p.vy<-2){RT.tut=3;AU.sfx('power');
-   TTS.say(STR.tutorial.gatePraise);}}
+   TTS.say(tr().tutorial.gatePraise);}}
  else if(RT.tut===3){
-  RT.curHint=STR.tutorial.gateHint;RT.hintTimer=200;}}
+  RT.curHint=tr().tutorial.gateHint;RT.hintTimer=200;}}
 
 export function tutorialComplete(){
  if(RT.tut===3){RT.tut=0;S.tutorial=true;save();}}
