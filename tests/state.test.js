2feaedb7f4db002cc2ca6a37785f52b7be4f1cc5
dudududu144 <dudududi144@gmail.com>
2026-08-22 11:17:23 +0300

    test(M1): skillModel default state assertion

--- tests/state.test.js
+++ tests/state.test.js
@@ -13,6 +13,7 @@ describe('state',()=>{
   expect(d.diff).toBe('רגיל');
   expect(d.mode).toBe('חוקר');
   expect(d.tutorial).toBe(false);
+  expect(d.skillModel).toEqual({});
   expect(d.best).toBe(0);});
  it('שלוש רמות קושי עם ערכים שפויים',()=>{
   expect(Object.keys(DIFF)).toEqual(['קל','רגיל','מאתגר']);
