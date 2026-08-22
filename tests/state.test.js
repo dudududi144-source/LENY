cb25a9d6f3983a4f8f3be8508dd8815f15da814d
dudududu144 <dudududi144@gmail.com>
2026-08-22 12:55:15 +0300

    test(M2): garden default state assertion

--- tests/state.test.js
+++ tests/state.test.js
@@ -14,6 +14,7 @@ describe('state',()=>{
   expect(d.mode).toBe('חוקר');
   expect(d.tutorial).toBe(false);
   expect(d.storySeen).toBe(false);
+  expect(d.garden).toEqual([]);
   expect(d.skillModel).toEqual({});
   expect(d.best).toBe(0);});
  it('שלוש רמות קושי עם ערכים שפויים',()=>{
