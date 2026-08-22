f7b64b0d2b1048f05e6ed64a91d1254dbb6a3cf4
dudududu144 <dudududi144@gmail.com>
2026-08-22 10:46:40 +0300

    test(M0): tutorial default flag assertion

--- tests/state.test.js
+++ tests/state.test.js
@@ -12,6 +12,7 @@ describe('state',()=>{
   expect(d.night).toBe(false);
   expect(d.diff).toBe('רגיל');
   expect(d.mode).toBe('חוקר');
+  expect(d.tutorial).toBe(false);
   expect(d.best).toBe(0);});
  it('שלוש רמות קושי עם ערכים שפויים',()=>{
   expect(Object.keys(DIFF)).toEqual(['קל','רגיל','מאתגר']);
